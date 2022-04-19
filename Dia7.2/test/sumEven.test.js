e7af6189c6779d18211b4ad78f2d643293b90424
Carlos Eduardo <cpwaldow@gmail.com>
2022-04-19 18:10:17 -0300

    removendo a resposta do teste sumEven

--- Dia7.2/test/sumEven.test.js
+++ Dia7.2/test/sumEven.test.js
@@ -2,14 +2,12 @@ const sumEven = require('../src/sumEven.js');
 
 describe('Soma de números pares que vem no array', () => {
   it('Retorne a soma de todos os números pares', () => {
-    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    expect(sumEven(arr)).toEqual(30);
+    fail('Remova essa linha e escreva seu teste aqui');
   });
   it('Retorne `0` se o array não tenha ao menos um número par', () => {
-    const arr = [1, 3, 5, 7, 9, 11];
-    expect(sumEven(arr)).toEqual(0);
+    fail('Remova essa linha e escreva seu teste aqui');
   });
   it('Retorne `0` se o arâmetro da função estiver vazio', () => {
-    expect(sumEven()).toEqual(0);
+    fail('Remova essa linha e escreva seu teste aqui');
   });
 });
