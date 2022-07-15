3c410a9036aa8ffd14b86035c59a8d9f8a930f86
washington gama <wgamaneto16@gmail.com>
2022-07-15 09:10:33 -0300

    parte 2, questao 3

--- script-parte2.js
+++ script-parte2.js
@@ -1,8 +1,17 @@
-// Questao 1
-const fatorial = (numero) => numero > 1 ? numero * fatorial(numero - 1) : 1;
-console.log(fatorial(7));
+// // Questao 1
+// const fatorial = (numero) => numero > 1 ? numero * fatorial(numero - 1) : 1;
+// console.log(fatorial(7));
 
-// Qustao 2
-const maiorPalavra = (texto) => texto.split(' ').sort((a, b) => b.length - a.length)[0];
+// // Qustao 2
+// const maiorPalavra = (texto) => texto.split(' ').sort((a, b) => b.length - a.length)[0];
 
-console.log(maiorPalavra('Antonio foi ao banheiro e não sabemos o que aconteceu'));
+// console.log(maiorPalavra('Antonio foi ao banheiro e não sabemos o que aconteceu'));
+
+//Questao 3
+const contagem = document.getElementById('contadorClicks');
+let click = 0
+
+contagem.addEventListener('click', () => {
+    click += 1;
+    console.log(click);
+})
