f179fc45a3d475e1dedd7369dd64d41f93a03ed5
washington gama <wgamaneto16@gmail.com>
2022-07-19 09:09:08 -0300

    atividades 7.3

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,19 @@
+//Questao 1
+
 const sum = require("./sum");
 
-test(sum), () => {
-    expect(4, 5).toBe(9)
-}
+describe('Teste da questao 1 com sum()', () => {
+    it('Testa se o retorno de sum(4, 5) e 9', () => {
+        expect(sum(4, 5)).toBe(9)
+    });
+
+    it('Testa se o retorno de sum(0, 0) e 0', () => {
+        expect(sum(0, 0)).toBe(0)
+    });
+
+    it('Testa se retorna um erro quando os para,etros sao 4 e "5"', () => {
+        expect(() => sum(4, `5`)).toThrowError();
+        expect(() => sum(4, `5`)).toThrowError("parameters must be numbers");
+    });
+});
+
