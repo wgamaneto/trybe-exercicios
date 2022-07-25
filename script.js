02da38d2aa30fbc02ce8edd812136564eb123a67
washington gama <wgamaneto16@gmail.com>
2022-07-25 20:00:49 -0300

    Filter e Map

--- script.js
+++ script.js
@@ -1,49 +1,106 @@
-const employeeGenerator = (fullName) => {
-    const email = fullName.toLowerCase().split(' ').join('_');
-    return { fullName, email: `${email}@trybe.com` };
-  };
-  
-  const newEmployees = (callback) => {
-    const employees = {
-      id1: callback('Pedro Guerra'),
-      id2: callback('Luiza Drumond'),
-      id3: callback('Carla Paiva'),
-    };
-    return employees;
-  };
-  
-  console.log(newEmployees(employeeGenerator));
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+//Questao 1
+const info = () => {
+  return books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`)
+}
+
+console.log(info(books));
 
 
-const numberChecker = (myNumber, number) => myNumber === number;
-
-const lotteryResult = (myNumber, callback) => {
-  const number = Math.floor((Math.random() * 5) + 1);
-
-  return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try Again!';
-};
-
-console.log(lotteryResult(2, numberChecker));
+//Questao 2
+const objetosArr = () => {
+  return books.map((book) => ({ author: book.author.name, age: book.releaseYear - book.author.birthYear })).sort((a, b) => a.age - b.age)
+}
+console.log(objetosArr(books));
 
 
-const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
-const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
-const compareAnswers = (rightAnswer, studentAnswer) => {
-  if (rightAnswer === studentAnswer) {
-    return 1;
-  } if (studentAnswer === 'N.A') {
-    return 0;
-  }
-  return -0.5;
-};
+//Questao3
+const filtrarGenero = () => {
+  return books.filter((filtro) => filtro.genre === 'Ficção Científica' || filtro.genre === 'Fantasia')
+}
 
-const countPoints = (rightAnswers, studentAnswers, action) => {
-  let contador = 0;
-  for (let index = 0; index < rightAnswers.length; index += 1) {
-    const actionReturn = action(rightAnswers[index], studentAnswers[index]);
-    contador += actionReturn;
-  }
-  return `Resultado final: ${contador} pontos`;
-};
+console.log(filtrarGenero(books));
 
-console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
+
+//Questao 4
+const idadeLivros = () => {
+  return books.filter((idade) => 2022 - idade.releaseYear > 60).sort((a, b) => a.releaseYear - b.releaseYear)
+}
+console.log(idadeLivros(books));
+
+
+//Questao 5
+const expectedResult = [
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
+];
+
+const autores = () => {
+  const generos = ['Fantasia', 'Ficção Científica'];
+  return expectedResult.filter((book) => generos.includes(book.genre)).map((book) => book.author.name).sort();
+}
+console.log(autores(books));
